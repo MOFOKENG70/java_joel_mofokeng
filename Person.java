2a2005dddbcd44d8905dff159ebd0abcbec4d17f
MOFOKENG70 <joelmofokeng70@gmail.com>
2023-03-09 15:02:13 +0200

    person class activity

---
+++ Person.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;       //use this or always qualify the name like java.util.Scanner.Scanner
+
+public class Person{                                 
+    public static void main(String[] args){  
+    
+    int Age = 23;
+    Double Height = 1.70;                // In metres
+    String Skin_Color = "Brown";
+    
+
+    System.out.println("The persons age is: "+Age);
+    System.out.println("The persons height is: "+Height+"m");
+    System.out.println("The persons skin color is: "+Skin_Color);
+    
+    }
+}
